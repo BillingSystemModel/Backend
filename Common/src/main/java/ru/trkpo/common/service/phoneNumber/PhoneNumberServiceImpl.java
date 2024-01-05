8febaf62d4fb24a7e5fc261b5d01d2652d29e953
Artem Kurdikov <kourtema@gmail.com>
2024-01-05 14:27:56 +0300

    Add: CRM data endpoints
    Endpoints: /crm/tarifficate, /crm/pay, /crm/changeTariff, /crm/report

--- Common/src/main/java/ru/trkpo/common/service/phoneNumber/PhoneNumberServiceImpl.java
+++ Common/src/main/java/ru/trkpo/common/service/phoneNumber/PhoneNumberServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.trkpo.common.service.phoneNumber;
 
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -7,22 +8,22 @@ import org.springframework.transaction.support.TransactionTemplate;
 import ru.trkpo.common.data.entity.PhoneNumber;
 import ru.trkpo.common.data.entity.Tariff;
 import ru.trkpo.common.exception.NoDataFoundException;
+import ru.trkpo.common.service.tariff.TariffRepository;
+
+import java.math.BigDecimal;
 
 @Service
+@AllArgsConstructor
 public class PhoneNumberServiceImpl implements PhoneNumberService {
 
-    private final PhoneNumberRepository repository;
+    private final PhoneNumberRepository phoneNumberRepository;
+    private final TariffRepository tariffRepository;
     private final TransactionTemplate transactionTemplate;
-
-    public PhoneNumberServiceImpl(PhoneNumberRepository repository, TransactionTemplate transactionTemplate) {
-        this.repository = repository;
-        this.transactionTemplate = transactionTemplate;
-    }
 
     @Override
     public PhoneNumber findByPhoneNumber(String phoneNumber) {
         return transactionTemplate.execute(status ->
-                repository.findByPhoneNumber(phoneNumber).orElseThrow(
+                phoneNumberRepository.findByPhoneNumber(phoneNumber).orElseThrow(
                         () -> new NoDataFoundException("No such client")
                 )
         );
@@ -31,9 +32,9 @@ public class PhoneNumberServiceImpl implements PhoneNumberService {
     @Override
     public PhoneNumber findRandom() {
         return transactionTemplate.execute(status -> {
-            long clientNum = repository.count();
+            long clientNum = phoneNumberRepository.count();
             int pageNumber = (int)(Math.random() * clientNum);
-            Page<PhoneNumber> page = repository.findAll(PageRequest.of(pageNumber, 1));
+            Page<PhoneNumber> page = phoneNumberRepository.findAll(PageRequest.of(pageNumber, 1));
             if (! page.hasContent())
                 return null;
             return page.getContent().get(0);
@@ -43,10 +44,36 @@ public class PhoneNumberServiceImpl implements PhoneNumberService {
     @Override
     public Tariff findActiveTariff(String phoneNumber) {
         return transactionTemplate.execute(status -> {
-            PhoneNumber phone = repository.findByPhoneNumber(phoneNumber).orElseThrow(
+            PhoneNumber phone = phoneNumberRepository.findByPhoneNumber(phoneNumber).orElseThrow(
                     () -> new NoDataFoundException("No such client")
             );
             return phone.getTariff();
+        });
+    }
+
+    @Override
+    public BigDecimal updateBalance(String phoneNumber, BigDecimal money) {
+        return transactionTemplate.execute(status -> {
+            PhoneNumber phone = phoneNumberRepository.findByPhoneNumber(phoneNumber).orElseThrow(
+                    () -> new NoDataFoundException("No such client")
+            );
+            phone.setBalance(phone.getBalance().add(money));
+            phoneNumberRepository.updatePhoneNumberBalance(phone.getBalance(), phone.getClientId());
+            return phone.getBalance();
+        });
+    }
+
+    @Override
+    public String changeTariff(String phoneNumber, String tariffCode) {
+        return transactionTemplate.execute(status -> {
+            PhoneNumber phone = phoneNumberRepository.findByPhoneNumber(phoneNumber).orElseThrow(
+                    () -> new NoDataFoundException("No such client")
+            );
+            Tariff tariff = tariffRepository.findByIdEquals(tariffCode).orElseThrow(
+                    () -> new NoDataFoundException("No such tariff")
+            );
+            phoneNumberRepository.updatePhoneNumberTariff(tariff, phone.getClientId());
+            return tariff.getId();
         });
     }
 }
