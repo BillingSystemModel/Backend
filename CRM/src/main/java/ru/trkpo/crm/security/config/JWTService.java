b740ea5c11a8f71b02d557385805bde94c32f87c
Artem Kurdikov <kourtema@gmail.com>
2024-02-06 23:30:45 +0300

    Update: project dependencies
    1. java 17 -> 21
    2. spring-boot 3.1.5 -> 3.2.2
    3. spring-cloud 4.0.4 -> 4.1.0
    4. jwt token (jjwt) 0.11.5 -> 0.12.5

--- CRM/src/main/java/ru/trkpo/crm/security/config/JWTService.java
+++ CRM/src/main/java/ru/trkpo/crm/security/config/JWTService.java
@@ -2,14 +2,13 @@ package ru.trkpo.crm.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -52,24 +51,24 @@ public class JWTService {
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
        return Jwts.builder()
-               .setClaims(extraClaims)
-               .setSubject(userDetails.getUsername())
-               .setIssuedAt(new Date(System.currentTimeMillis()))
-               .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
-               .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+               .claims(extraClaims)
+               .subject(userDetails.getUsername())
+               .issuedAt(new Date(System.currentTimeMillis()))
+               .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+               .signWith(getSignInKey(), Jwts.SIG.HS256)
                .compact();
     }
 
     private Claims extractAllClaims(String token) {
         return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())
+                .parser()
+                .verifyWith(getSignInKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
-    private Key getSignInKey() {
+    private SecretKey getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtSecretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
