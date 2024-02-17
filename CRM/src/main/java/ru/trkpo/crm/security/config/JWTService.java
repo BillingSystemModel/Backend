7f3e08a351803bc114e884aadc83a33a5762aa27
Artem Kurdikov <kourtema@gmail.com>
2024-02-17 16:23:28 +0300

    Add: AuthServiceTest and JWTServiceTest

--- CRM/src/main/java/ru/trkpo/crm/security/config/JWTService.java
+++ CRM/src/main/java/ru/trkpo/crm/security/config/JWTService.java
@@ -37,8 +37,13 @@ public class JWTService {
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        try {
+            final String username = extractUsername(token);
+            return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        } catch (Exception e){
+            return false;
+        }
+
     }
 
     private boolean isTokenExpired(String token) {
